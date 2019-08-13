db1a548d7f242dfcce8614a17a1b719058f47053
Arjun Aravind <arjun.aravind1998@gmail.com>
2019-08-13 20:53:49 +0000

    Start work on RegExp in Data-Validation.php

--- src/candidate-listings.php
+++ src/candidate-listings.php
@@ -9,18 +9,35 @@
 /* We import the necessary php libraries. */
 require 'sql-connections.php';
 require 'sql-functions.php';
+require 'data-validation.php';
 
 /*-----------*/
 /* Insertion */
 /*-----------*/
 
-function insertCandidate(){
+/* This function inserts given candidate info into 'Unplaced Candidates' table. */
+function insertCandidate($candidateInfo){
+
+	/* We must first validate the information. */
+
+	$autoIncValue = getCurrentAutoIncValue().".pdf";
+	$rejectedCompanies = "0000 ";
 
 	try {
 
-		$conn = getConnection();
-		$sqlstmt = "INSERT INTO ";
+		$connection = getConnection();
+		$sqlstmt = "INSERT INTO UNPLACED_CANDIDATES VALUES (ID, ?, ?, ?, ?, ?, ?);";
 
+		$sql = $connection->prepare($sqlstmt);
+		$sql->bindParam(1, $candidateInfo[0]);
+		$sql->bindParam(2, $candidateInfo[1]);
+		$sql->bindParam(3, $candidateInfo[2]);
+		$sql->bindParam(4, $candidateInfo[3]);
+		$sql->bindParam(5, $autoIncValue);
+		$sql->bindParam(6, $rejectedCompanies);
+		$sql->execute();
+
+		$connection = NULL;
 
 	} catch (PDOException $exception) {
 		echo "Exception Thrown (candidate-listings.php/insertCandidate): $exception";
@@ -28,19 +45,19 @@ function insertCandidate(){
 
 }
 
+insertCandidate(array("Arjun","8939227284","arjun@gmail.com","DIPLOMA"));
 
 /*-----------*/
 /* Utilities */
 /*-----------*/
 
-
 /* This function returns the current Auto-Increment value from the given table. */
-function getCurrentAutoIncValue($tableName){
+function getCurrentAutoIncValue(){
 
 	try {
 
 		$conn = getConnection(); //Creates connection.
-		$sqlstmt = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'BUILDERS_ASSOCIATION' AND TABLE_NAME = '$tableName';";
+		$sqlstmt = "SELECT AUTO_INCREMENT FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'BUILDERS_ASSOCIATION' AND TABLE_NAME = 'UNPLACED_CANDIDATES';";
 
 		$results = executeQuery($conn, $sqlstmt);
 		$conn = NULL; //Closes connection.
@@ -49,10 +66,9 @@ function getCurrentAutoIncValue($tableName){
 
 	} catch (PDOException $exception) {
 		echo "Exception Thrown (candidate-listings.php/getCurrentAutoIncValue): $exception";
+		return -1;
 	}
 
 }
-
-echo getCurrentAutoIncValue();
 
 ?>
---
+++ src/data-validation.php
@@ -0,0 +1,15 @@
+<?php
+
+/*************************/
+/** DATA-VALIDATION.PHP **/
+/*************************/
+
+function numberIsValid($number) {
+
+	if(strlen($number) == 10 || strlen($number) == 8) {
+		if(
+	}
+
+}
+
+?>
