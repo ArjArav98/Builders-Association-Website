41fba5dfe34535472f4a2ba994dcb3c9ebc7374b
Arjun Aravind <arjun.aravind1998@gmail.com>
2019-08-20 03:35:02 +0000

    Add LOGIN module.
    Added a login module which will authenticate users entering the application.

---
+++ src/login.php
@@ -0,0 +1,48 @@
+<?php
+
+require 'sql-connections.php';
+require 'sql-functions.php';
+
+/*************/
+/* LOGIN.PHP */
+/*************/
+
+/* This file contains information and functions for logging in. */
+
+/* This function checks whether a given username/password are valid. */
+function login($username, $password, $isCompany) {
+
+	/* If the login is attempted by the admin... */
+	if($isCompany == false) {
+		if($username == "admin" && $password == "admin") { //Default login.
+			return true;
+		}
+		return false;
+	}
+
+	/* If the login is attempted by a company...  */
+
+	try {
+
+		$connection = getConnection();
+		$sqlstmt = "SELECT COUNT(*) AS LOGIN FROM COMPANIES WHERE USERNAME='$username' AND PASSWORD='$password';";
+
+		$results = executeQuery($connection, $sqlstmt);
+		$connection = NULL; //Closes connection.
+
+		if($results[0]['LOGIN'] == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	} catch (PDOEXception $exception) {
+		echo "Exception Thrown (login.php/login): $exception";
+	}
+
+	return false; //If something goes wrong.
+
+}
+
+?>
