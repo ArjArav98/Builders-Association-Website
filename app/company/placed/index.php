9d3821f4c8858ff3360b41b9f68372b3f09e546e
arjun1098 <arjun.aravind1998@gmail.com>
2019-09-05 16:10:46 +0530

    Finish COMPANY-PLACED module.
    Finished the module which deals with displaying placed candidates for the company.

--- app/company/placed/index.php
+++ app/company/placed/index.php
@@ -11,7 +11,7 @@ session_start();
 require '../../../src/candidate-listings.php';
 
 /* We get the required list of candidates from the database using the search options. */
-$results = getCandidate(NULL, NULL, NULL, $_SESSION['COMPANY_ID'], 0, 0, 1);
+$results = getCandidate(NULL, NULL, NULL, $_SESSION['COMPANY_ID'], 1, 0, 1);
 
 $iterator = 0;
 $length = sizeof($results[0]);
@@ -22,7 +22,6 @@ while($iterator < $length) {
 
 	$HTML .= "<div class='candidate'>";
 	$HTML .= "<p><span class='cell-value cell-value-bg alternative-cl'>".$results[1][$iterator]."</span><span class='cell-value'>".$results[2][$iterator]."</span><span class='cell-value alternative-cl'>".$results[3][$iterator]."</span><span class='cell-value cell-value-bg'>".$results[4][$iterator]."</span></p>";
-	$HTML .= "<form method='get' action='place-candidate.php' class='candidate-form place'><input type='hidden' name='candidateId' value='".$results[0][$iterator]."'><button type='submit'>Place</button></form><form method='get' action='reject-candidate.php' class='candidate-form reject'><input type='hidden' name='candidateId' value='".$results[0][$iterator]."'><button type='submit'>Reject</button></form>";
 	$HTML .= "</div>";
 
 	$iterator += 1;
@@ -37,7 +36,7 @@ while($iterator < $length) {
 <!DOCTYPE html>
 
 	<head>
-		<title>Companies - Home | Builders Association</title>
+		<title>Companies - Placed Candidates | Builders Association</title>
 		<link href="static/css/index1.css" rel="stylesheet">
 	</head>
 
@@ -46,12 +45,12 @@ while($iterator < $length) {
 		<!-- This page will have a list of referred candidates for the company. -->
 
 		<nav>
-			<a href="">Referred Candidates</a><a
-			href="../placed/index.php">Placed Candidates</a><a
+			<a href="../home/index.php">Referred Candidates</a><a
+			href="">Placed Candidates</a><a
 			href="#">Logout</a>
 		</nav>
 
-		<h2>Referred Candidates</h2>
+		<h2>Candidates Placed</h2>
 
 		<!-- The list of candidates will be displayed here. -->
 		<div class="candidate-list">
---
+++ app/company/placed/place-candidate.php
@@ -0,0 +1,17 @@
+<?php
+
+/***********************/
+/* PLACE-CANDIDATE.PHP */
+/***********************/
+
+session_start();
+require '../../../src/candidate-listings.php';
+
+$candidateId = $_GET['candidateId'];
+
+placeCandidate($candidateId);
+
+header('Location: index.php', true, 303);
+die();
+
+?>
