022d34942100e9a6123527ef83c4b38f6eb6c343
arjun1098 <arjun.aravind1998@gmail.com>
2019-10-14 11:48:15 +0530

    Finish documentation/code for resume/ module.

--- app/candidate/register/register.php
+++ app/candidate/register/register.php
@@ -1,11 +1,19 @@
 <?php
 
+/****************/
+/* REGISTER.PHP */
+/****************/
+
+/* This file takes data from the candidate/register module and inserts into the table. */
+
+/* We first include the necessary libraries. */
 require '../../../src/sql-connections.php';
 require '../../../src/sql-functions.php';
 require '../../../src/data-validation.php';
 require '../../../src/candidate-listings.php';
 require '../../../src/mail.php';
 
+/* This takes the data from the form. */
 $name = $_POST['name'];
 $number = $_POST['number'];
 $email = $_POST['email'];
@@ -13,19 +21,20 @@ $qualification = $_POST['qualification'];
 $experience = $_POST['experience'];
 $district = $_POST['district'];
 
+/* We insert the data into the table and send the email to the candidate's email. */
 insertCandidate($name, $number, $email, $qualification, $experience, $district);
 sendInfoEmailToCandidate($email);
 
-if(!empty($_FILES['resume'])) //This gets the data from the 'choose file' field.
-{
-  $path = "resumes/";
-  $path = $path . getCurrentAutoIncValue();
-  if(move_uploaded_file($_FILES['resume']['tmp_name'], $path)){ }
-  else{
-      echo "There was an error uploading the file, please try again!";
-  }
+/* We move the chosen resume to the resumes/ folder. */
+if(!empty($_FILES['resume'])) {
+	$path = "resumes/";
+	$path = $path . getCurrentAutoIncValue();
+	
+	if(move_uploaded_file($_FILES['resume']['tmp_name'], $path)){ }
+	else { }
 }
 
+/* We then redirect to the index.html file. */
 header('Location: index.html', true, 303);
 die();
 
